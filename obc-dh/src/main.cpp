d5ab4d15eb5e4a2ca8434dcac12aee179360ad64
Davi Lacerda <lcrd475@gmail.com>
2026-08-09 19:29:20 -0300

    obc-dh: Configuração do barramento e Instâncias de Hardware

--- obc-dh/src/main.cpp
+++ obc-dh/src/main.cpp
@@ -4,34 +4,73 @@
 #include <HardwareSerial.h>
 
 // Pinagem
-
+#define LED_BUILTIN 2
 // UART - RPi zero W
-#define RXpld 16
-#define TXpld 17
+#define PIN_RXpld 16
+#define PIN_TXpld 17
 
 // I2C - TC, EPS e ADCS
-#define SDAitc 21
-#define SCLitc 22
+#define PIN_SDAitc 21
+#define PIN_SCLitc 22
 
 // VSPI - LoRa SX1276 TT&C 
-#define SCKv 18
-#define MISOv 19
-#define MOSIv 23
-#define CSv 5
-#define RSTv 14
-#define DIO0v 4
+#define PIN_SCKv 18
+#define PIN_MISOv 19
+#define PIN_MOSIv 23
+#define PIN_CSv 5
+#define PIN_RSTv 14
+#define PIN_DIO0v 4
 
 // HSPI - SD card OBC
-#define SCKh 15
-#define MISOh 35 // Input-only
-#define MOSIh 12
-#define CSh 13
+#define PIN_SCKh 15
+#define PIN_MISOh 35 // Input-only
+#define PIN_MOSIh 12
+#define PIN_CSh 13
 
 // PWM - SimpleFOC
-#define PWMu 25
-#define PWMv 26
-#define PWMw 27
-#define EN 32
+#define PIN_PWMu 25
+#define PIN_PWMv 26
+#define PIN_PWMw 27
+#define PIN_EN 32
 
 // Burn wire - Gatilho
-#define BURN 33
+#define PIN_BURN 33
+
+// INSTANCIAS
+HardwareSerial SerialPLD(2); // UART - RPi zero W
+SPIClass SPIh(HSPI); // HSPI - SD card
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.begin(115200);
+
+  unsigned long startWait = millis();
+  while (!Serial && (millis() - startWait < 3000))
+  {
+    delay(100);
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // Pisca o LED enquanto espera a inicialização da Serial
+  }
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("\nOBC FCP-01 Inicializando");
+  
+  pinMode(PIN_BURN, OUTPUT);
+  digitalWrite(PIN_BURN, LOW); // Gatilho do Burn Wire desligado
+
+  pinMode(PIN_EN, OUTPUT);
+  digitalWrite(PIN_EN, LOW); // Driver do motor desligado
+
+  // Inicializa UART2
+    SerialPLD.begin(115200, SERIAL_8N1, PIN_RXpld, PIN_TXpld); // Inicializa UART2 com baudrate de 115200, 8 bits de dados, sem paridade e 1 bit de parada
+    Wire.begin(PIN_SDAitc, PIN_SCLitc, 400000); // Inicializa I2C com frequência de 400kHz
+    SPIh.begin(PIN_SCKh, PIN_MISOh, PIN_MOSIh, PIN_CSh); // Inicializa HSPI com pinos definidos
+    SPI.begin(PIN_SCKv, PIN_MISOv, PIN_MOSIv, PIN_CSv); // Inicializa VSPI com pinos definidos
+    Serial.println("Interfaces de comunicação inicializadas");
+
+
+}
+void loop() {
+  // Coloque aqui o código principal do seu programa
+}
