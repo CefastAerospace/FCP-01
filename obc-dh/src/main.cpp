6bdce14f2a134db96b99422fb3796540c5195232
Davi Lacerda <lcrd475@gmail.com>
2026-08-09 18:19:07 -0300

    obc-dh: pinagem e adição de libs

--- obc-dh/src/main.cpp
+++ obc-dh/src/main.cpp
@@ -9,7 +9,29 @@
 #define RXpld 16
 #define TXpld 17
 
-// I2C TT&C, EPS e ADCS
-#define SDA 21
-#define SCL 22
+// I2C - TC, EPS e ADCS
+#define SDAitc 21
+#define SCLitc 22
 
+// VSPI - LoRa SX1276 TT&C 
+#define SCKv 18
+#define MISOv 19
+#define MOSIv 23
+#define CSv 5
+#define RSTv 14
+#define DIO0v 4
+
+// HSPI - SD card OBC
+#define SCKh 15
+#define MISOh 35 // Input-only
+#define MOSIh 12
+#define CSh 13
+
+// PWM - SimpleFOC
+#define PWMu 25
+#define PWMv 26
+#define PWMw 27
+#define EN 32
+
+// Burn wire - Gatilho
+#define BURN 33
