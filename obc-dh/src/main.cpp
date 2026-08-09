dde83f7fab43ead0dff70cfbbd20be2b469ff856
Davi Lacerda <lcrd475@gmail.com>
2026-08-09 18:06:27 -0300

    obc-dh: Começo da pinagem e adição de libs

---
+++ obc-dh/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <HardwareSerial.h>
+
+// Pinagem
+
+// UART - RPi zero W
+#define RXpld 16
+#define TXpld 17
+
+// I2C TT&C, EPS e ADCS
+#define SDA 21
+#define SCL 22
+
