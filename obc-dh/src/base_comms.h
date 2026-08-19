4adb88583c0dacceb7f63bc05c1c5b86ec5b166d
Daniel Bosco <dani.b.velloso@gmail.com>
2026-08-19 08:48:17 -0300

    obc-dh: Create base_comms.h

---
+++ obc-dh/src/base_comms.h
@@ -0,0 +1,22 @@
+#ifndef BASE_COMMS_H
+#define BASE_COMMS_H
+#include <Arduino.h>
+#include "sd_logger.h"
+
+// UART - RPI ZERO W
+#define PIN_RXpld 16
+#define PIN_TXpld 17
+
+#define BASE_BAUDRATE 115200
+
+// QUEUE
+#define BASE_QUEUE_LENGTH 20
+
+// INICIALIZAÇÃO
+bool Base_Init();
+bool Base_StartTask();
+
+// ENVIO
+bool Base_SendPacket(const LogPacket& packet);
+
+#endif
